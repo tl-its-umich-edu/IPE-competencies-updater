5bb6c4e4634ad00c496785579ec9ed097212a05f
pushyami <pushyami@umich.edu>
2021-11-04 17:12:52 -0400

    better wau of organizing test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,17 @@
+from typing import List
 import pytest
 import pandas as pd
 from dotenv import load_dotenv
 from read_env_props import ReadEnvProps
 from ipe_course_data.get_ipe_data_from_gsheets import GetIPEDataFromSheets
 
-@pytest.fixture(scope="session")
+
+
+pytest_plugins = [
+    "tests.fixtures.worksheets_columns"
+]
+
+@pytest.fixture(scope="session", autouse=True)
 def ipe_props():
     """
     Fixture for Env properties
@@ -13,8 +20,8 @@ def ipe_props():
     envProps: ReadEnvProps =  ReadEnvProps()
     return envProps.get_env_props()
 
-@pytest.fixture(scope="session")
-def ipe_workbook(ipe_props):
+@pytest.fixture(scope="session", autouse=True)
+def ipe_workbook(ipe_props)->List[List[str]]:
     """
     fixture for IPE workbook
     """
@@ -22,51 +29,14 @@ def ipe_workbook(ipe_props):
     ws = GetIPEDataFromSheets(ipe_props).get_data().get_all_records()
     return ws
 
-@pytest.fixture(scope="session")
-def ipe_ws_df(ipe_workbook):
+@pytest.fixture(scope="session", autouse=True)
+def ipe_ws_df(ipe_workbook)->pd.DataFrame:
     """
     fixture for sample dataframe
     """
     df = pd.DataFrame(ipe_workbook)
     return df
 
-@pytest.fixture(scope="session")
-def worksheet_columns():
-    """
-    Sample worksheet columns with leading and trailing whitespaces
-    """
-    return ['Offering Name', 'Offering ID', 'Offering-instance ID', 'Year', 'Term',
-       'UM Term Code', 'udp_term_name', 'Description', 'Lead Faculty Member 1',
-       'Lead Faculty Member 2', 'Lead Faculty 1 School',
-       'Lead Faculty 2 School', 'Lead Faculty 1 unique name',
-       'Lead Faculty 2 unique name', 'Format',
-       'Instructional Team (SEE LOOKUP TABLE)',
-       'Program offering to students (SEE LOOKUP TABLE)',
-       'Course #s (if applicable)', 'Window used', 'IPE Assessments',
-       'Canvas Site or Shell Course', 'Canvas Course ID',
-       'Roles/ Responsibilities', 'Interprofessional Communication',
-       'Teams/Teamwork', 'Values/Ethics', 'Intercultural Humility',
-       'Dosage (contact hours)', 'Criteria for Assigning Outcomes in Canvas',
-       'Notes', 'When does script run? (Feb, June, Oct)', 'Script Run?']
-
-@pytest.fixture(scope="session")
-def worksheet_columns_with_whitespace():
-    """
-    This test make sure that ipe spread sheets columns whitespaces are trimmed
-    """
-    return pd.Index(['Offering Name', 'Offering ID', 'Offering-instance ID', 'Year', 'Term ',
-       'UM Term Code', 'udp_term_name', 'Description', 'Lead Faculty Member 1',
-       'Lead Faculty Member 2', 'Lead Faculty 1 School',
-       'Lead Faculty 2 School', 'Lead Faculty 1 unique name',
-       'Lead Faculty 2 unique name', 'Format',
-       'Instructional Team (SEE LOOKUP TABLE)',
-       'Program offering to students (SEE LOOKUP TABLE)',
-       'Course #s (if applicable)', 'Window used', 'IPE Assessments',
-       'Canvas Site or Shell Course', 'Canvas Course ID',
-       'Roles/ Responsibilities ', 'Interprofessional Communication ',
-       'Teams/Teamwork ', 'Values/Ethics ', 'Intercultural Humility',
-       'Dosage (contact hours) ', 'Criteria for Assigning Outcomes in Canvas',
-       'Notes', 'When does script run? (Feb, June, Oct)', 'Script Run?'])
     
 
 
